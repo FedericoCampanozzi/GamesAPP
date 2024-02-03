3ebd59d3147050f2dcfbbd46e8d545f4cef8e766
Federico Campanozzi <federico.campanozzi@gmail.com>
2024-02-03 14:18:38 +0100

    Added User table

--- GamesAPP/GamesAPP.Shared/Data/DataContext.cs
+++ GamesAPP/GamesAPP.Shared/Data/DataContext.cs
@@ -11,5 +11,6 @@ namespace GamesAPP.Shared.Data
         }
 
         public DbSet<Product> Products { get; set; }
+		public DbSet<User> Users { get; set; }
     }
 }
---
+++ GamesAPP/GamesAPP.Shared/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesAPP.Shared.Entities
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public string UserName { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+		public string Role { get; set; } = string.Empty;
+	}
+}
