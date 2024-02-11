f3e2985d60a035d0befeeacb2f1c402c037f6f72
Federico Campanozzi <federico.campanozzi@gmail.com>
2024-02-11 23:49:08 +0100

    User cannot be null.

--- GamesAPP/GamesAPP.Shared/Entities/Order.cs
+++ GamesAPP/GamesAPP.Shared/Entities/Order.cs
@@ -14,12 +14,14 @@ namespace GamesAPP.Shared.Entities
 		public DateTime CreatedAt { get; set; } = DateTime.Now;
 		public int Quantity { get; set; } = -1;
 		public required Product Product { get; set; }
+		public required User User { get; set; }
 		public required Warehouse? Warehouse { get; set; }
-		public required User? User { get; set; }
 
 		public static Faker<Order> GetEntitySeeder(DataContext _data)
 		{
 			Random rnd = new Random();
+			List<User> storeUser = _data.Users.Where(u => u.Role == "Store").ToList();
+			List<User> user = _data.Users.Where(u => u.Role == "User").ToList();
 
 			if (rnd.Next(0, 2) == 0)
 				return new Faker<Order>()
@@ -27,13 +29,13 @@ namespace GamesAPP.Shared.Entities
 					.RuleFor(e => e.Quantity, f => rnd.Next(11))
 					.RuleFor(e => e.Product, f => f.PickRandom(_data.Products.ToArray()))
 					.RuleFor(e => e.Warehouse, f => null)
-					.RuleFor(e => e.User, f => f.PickRandom(_data.Users.ToArray()));
+					.RuleFor(e => e.User, f => f.PickRandom(user));
 			else
 				return new Faker<Order>()
 					.RuleFor(e => e.Quantity, f => rnd.Next(101))
 					.RuleFor(e => e.Product, f => f.PickRandom(_data.Products.ToArray()))
 					.RuleFor(e => e.Warehouse, f => f.PickRandom(_data.Warehouses.ToArray()))
-					.RuleFor(e => e.User, f => null);
+					.RuleFor(e => e.User, f => f.PickRandom(storeUser));
 		}
 
 		public static void DeleteAllItems(DataContext _data)
